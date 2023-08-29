75008308c57bdf18c128902ac901d5a1d4af9954
Jennifer Vasconcelos <64858624+jenniferVC@users.noreply.github.com>
2023-08-28 23:59:53 -0300

    OnibusLinha tela que mostra os onibus disponiveis de uma linha

---
+++ lib/screens/passageiro/onibus_linha.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import 'package:connect_bus/screens/onibus_localizacao.dart';
+
+/// Pagina responsavel por mostrar todos os onibus da respectiva linha
+/// que Usuario clicou na tela [ParadasScreen]
+
+class OnibusLinhaPage extends StatelessWidget {
+  final String nomeLinha;
+  const OnibusLinhaPage({
+    Key? key,
+    required this.nomeLinha,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Onibus"),
+        centerTitle: true,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('Onibus').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView(
+              children: snapshot.data!.docs.map((doc) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => OnibusLocalizacao(
+                                  codigoOnibus: doc.get('codigo'),
+                                )));
+                  },
+                  child: Card(
+                    child: ListTile(
+                      title: Text(doc.get('codigo')),
+                    ),
+                  ),
+                );
+              }).toList(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
