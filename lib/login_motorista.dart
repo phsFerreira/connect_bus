b06662264510eddca93117203a1d1b7466acefd3
Jennifer Vasconcelos <64858624+jenniferVC@users.noreply.github.com>
2023-08-29 00:50:10 -0300

    Rafatorando LoginMotorista

--- lib/login_motorista.dart
+++ lib/login_motorista.dart
@@ -1,43 +1,19 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_build_context_synchronously, unused_import
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:connect_bus/cadastro_passageiro.dart';
-import 'package:connect_bus/home_page.dart';
-import 'package:connect_bus/screens/maps.dart';
-import 'package:connect_bus/profile_motorista.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-class LoginPageMotorista extends StatefulWidget {
-  const LoginPageMotorista({Key? key}) : super(key: key);
+import 'package:connect_bus/profile_motorista.dart';
+
+class LoginMotoristaPage extends StatefulWidget {
+  const LoginMotoristaPage({Key? key}) : super(key: key);
 
   @override
-  State<LoginPageMotorista> createState() => _LoginPageMotoristaState();
+  State<LoginMotoristaPage> createState() => _LoginMotoristaPageState();
 }
 
-class _LoginPageMotoristaState extends State<LoginPageMotorista> {
+class _LoginMotoristaPageState extends State<LoginMotoristaPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-
-  Future signIn() async {
-    if (emailController.text.isEmpty | passwordController.text.isEmpty) {
-      Fluttertoast.showToast(
-          msg: 'Preencha todos os campos.',
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.BOTTOM,
-          backgroundColor: Colors.grey,
-          textColor: Colors.black);
-    } else {
-      FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailController.text.trim(),
-        password: passwordController.text.trim(),
-      );
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => const MotoristaPage()));
-    }
-  }
 
   @override
   void dispose() {
@@ -55,84 +31,92 @@ class _LoginPageMotoristaState extends State<LoginPageMotorista> {
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              //login text
               children: [
-                Icon(
-                  Icons.bus_alert,
-                  size: 40,
-                ),
-                Text(
-                  'Login',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
-                ),
-                SizedBox(height: 50),
-
-                //email textfield
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Container(
-                    decoration: BoxDecoration(color: Colors.white),
-                    child: TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(12)),
-                              borderSide:
-                                  BorderSide(color: Colors.black, width: 1)),
-                          hintText: 'Email',
-                          contentPadding: EdgeInsets.all(20.0)),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-
-                //password textfield
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Container(
-                    decoration: BoxDecoration(color: Colors.white),
-                    child: TextField(
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(12)),
-                              borderSide:
-                                  BorderSide(color: Colors.black, width: 1)),
-                          hintText: 'Senha',
-                          contentPadding: EdgeInsets.all(20.0)),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-
-                //login button
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                  child: GestureDetector(
-                    onTap: signIn,
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.all(Radius.circular(12))),
-                      child: Center(
-                          child: Text(
-                        'Login',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18),
-                      )),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 25),
+                _getIcon(),
+                _getTextLogin(),
+                const SizedBox(height: 50),
+                _getTextField(emailController, 'Email', false),
+                const SizedBox(height: 20),
+                _getTextField(passwordController, 'Senha', true),
+                const SizedBox(height: 20),
+                _getButtonLogin(),
+                const SizedBox(height: 25),
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  _getIcon() {
+    return const Icon(
+      Icons.bus_alert,
+      size: 40,
+    );
+  }
+
+  _getTextLogin() {
+    return const Text(
+      'Login',
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+    );
+  }
+
+  _getTextField(
+      TextEditingController? controller, String hintText, bool obscureText) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        decoration: const BoxDecoration(color: Colors.white),
+        child: TextField(
+          controller: controller,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+              border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  borderSide: BorderSide(color: Colors.black, width: 1)),
+              hintText: hintText,
+              contentPadding: const EdgeInsets.all(20.0)),
+        ),
+      ),
+    );
+  }
+
+  Future signIn() async {
+    if (emailController.text.isEmpty | passwordController.text.isEmpty) {
+      Fluttertoast.showToast(
+          msg: 'Preencha todos os campos.',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.grey,
+          textColor: Colors.black);
+    } else {
+      FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const HomeMotoristaPage()));
+    }
+  }
+
+  _getButtonLogin() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: GestureDetector(
+        onTap: signIn,
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.all(Radius.circular(12))),
+          child: const Center(
+              child: Text(
+            'Login',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+          )),
         ),
       ),
     );
