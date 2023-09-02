767e72037eb1503542575f04c5753b8b65b5c008
Jennifer Vasconcelos <64858624+jenniferVC@users.noreply.github.com>
2023-09-01 21:28:46 -0300

    documentação auth_page.dart

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:connect_bus/login_motorista.dart';
 import 'package:connect_bus/screens/codigo_onibus.dart';
 
+/// Controlador de Login, responsável por deixar o motorista logado no app,
+/// (funcionalidade fornecida pelo [FirebaseAuth]).
+
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
 
@@ -13,11 +16,19 @@ class AuthPage extends StatelessWidget {
       body: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (context, snapshot) {
+            // Enquanto estiver conectando ao Firebase, exiba um icone de carregamento.
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const CircularProgressIndicator();
-            } else if (snapshot.hasData) {
+            }
+            // Caso já Motorista ja tenha efetuado o login e não realizado o logout,
+            // irá redirecioná-lo para a pagina de codigo do onibus.
+            else if (snapshot.hasData) {
               return const CodigoOnibusPage();
-            } else {
+            }
+            // Caso Motorista não tenha realizado login nenhum vez no app,
+            // ou caso o Motorista clique do botão de logout em [HomeMotoristaPage]
+            //então o rediciona para a pagina de login.
+            else {
               return const LoginMotoristaPage();
             }
           }),
