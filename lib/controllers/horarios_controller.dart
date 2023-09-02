45b3439ebdc5f85a3f8cca65ae1cdb7dd6528111
Jennifer Vasconcelos <64858624+jenniferVC@users.noreply.github.com>
2023-09-02 16:37:04 -0300

    HorarioController função loadHorarios que encontra os horarios pelo nome do bairro

--- lib/controllers/horarios_controller.dart
+++ lib/controllers/horarios_controller.dart
@@ -1,9 +1,19 @@
+import 'package:connect_bus/model/horario.dart';
 import 'package:connect_bus/repositories/horarios_repository.dart';
 import 'package:flutter/material.dart';
 
 class HorariosController extends ChangeNotifier {
-  var horariosRepository = HorariosRepository();
-  loadHorarios(bairro) {
-    horariosRepository.findByBairroName(bairro);
+  List<ExpansionTile> expansionTiles = [];
+
+  loadHorarios(String bairro) async {
+    print('linhasHorariosDoBairro Controller ===>');
+    var horariosRepository = HorariosRepository();
+    List<Horario> linhasHorariosDoBairro =
+        await horariosRepository.findByBairroName(bairro);
+    print('linhasHorariosDoBairro Controller ===> $linhasHorariosDoBairro');
+
+    // (método de ChangeNotifier) usado para notificar
+    // qualquer um que esteja assistindo ParadasController
+    notifyListeners();
   }
 }
