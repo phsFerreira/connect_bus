f19bb9d806e1ffb81135c9120f739e1fdec452c9
Jennifer Vasconcelos <64858624+jenniferVC@users.noreply.github.com>
2023-09-02 16:41:19 -0300

    Criando Onibus Model

---
+++ lib/model/onibus.dart
@@ -0,0 +1,16 @@
+class Onibus {
+  String? codigo;
+  String? estadoFisico;
+  double? latitude;
+  double? longitude;
+  String? placa;
+  String? linha;
+  Onibus({
+    this.codigo,
+    this.estadoFisico,
+    this.latitude,
+    this.longitude,
+    this.placa,
+    this.linha,
+  });
+}
