499c0cc8fa2813d14e15b372f0318b4a7624b4ff
Jennifer Vasconcelos <64858624+jenniferVC@users.noreply.github.com>
2023-09-01 23:13:39 -0300

    Redesign de cadastro_passageiro.dart

--- lib/cadastro_passageiro.dart
+++ lib/cadastro_passageiro.dart
@@ -1,151 +1,165 @@
-// ignore_for_file: prefer_const_constructors, body_might_complete_normally_nullable, unused_local_variable
+import 'package:flutter/material.dart';
 
 import 'package:connect_bus/campo_form.dart';
 import 'package:connect_bus/extensoes.dart';
 import 'package:connect_bus/login_passageiro.dart';
-import 'package:flutter/material.dart';
-import 'passageiro.dart';
+import 'package:connect_bus/passageiro.dart';
+import 'package:connect_bus/widgets/button.dart';
 
-class CadastroPassageiroForm extends StatefulWidget {
-  const CadastroPassageiroForm({Key? key}) : super(key: key);
+class CadastroPassageiroPage extends StatefulWidget {
+  const CadastroPassageiroPage({Key? key}) : super(key: key);
 
   @override
-  State<CadastroPassageiroForm> createState() => _CadastroPassageiroFormState();
+  State<CadastroPassageiroPage> createState() => _CadastroPassageiroPageState();
 }
 
-class _CadastroPassageiroFormState extends State<CadastroPassageiroForm> {
+class _CadastroPassageiroPageState extends State<CadastroPassageiroPage> {
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: Center(
+          child: _getForm(),
+        ),
+      ),
+    );
+  }
 
+  _getForm() {
     String nome = "";
     String cpf = "";
     String telefone = "";
     String email = "";
     String senha = "";
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: OverflowBar(
+          overflowSpacing: 15,
+          children: <Widget>[
+            _getText(),
 
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      body: SafeArea(
-          child: Center(
-        child: SingleChildScrollView(
-          child: Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: Column(
-                  children: <Widget>[
-                    //texto
-                    Text(
-                      'Cadastro de Passageiro',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                    ),
-                    SizedBox(height: 30),
+            CampoForm(
+              hintText: 'Nome Completo',
+              validator: (value) {
+                nome = value.toString();
+                if (nome.isEmpty) {
+                  return 'Digite seu nome.';
+                } else if (nome.isValidName) {
+                  return 'Digite um nome válido.';
+                }
+                return null;
+              },
+            ),
 
-                    //nome completo
-                    CampoForm(
-                      hintText: 'Nome Completo',
-                      validator: (value) {
-                        nome = value.toString();
-                        if (nome.isEmpty) {
-                          return 'Digite seu nome.';
-                        } else if (nome.isValidName) {
-                          return 'Digite um nome válido.';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 20),
+            //CPF
+            CampoForm(
+              hintText: 'CPF',
+              validator: (value) {
+                cpf = value.toString();
+                if (cpf.isEmpty) {
+                  return 'Digite seu CPF.';
+                } else if (cpf.isValidCPF) {
+                  return 'Digite um CPF válido.';
+                }
+              },
+            ),
 
-                    //CPF
-                    CampoForm(
-                      hintText: 'CPF',
-                      validator: (value) {
-                        cpf = value.toString();
-                        if (cpf.isEmpty) {
-                          return 'Digite seu CPF.';
-                        } else if (cpf.isValidCPF) {
-                          return 'Digite um CPF válido.';
-                        }
-                      },
-                    ),
-                    SizedBox(height: 20),
+            //telefone
+            CampoForm(
+              hintText: 'Telefone',
+              validator: (value) {
+                telefone = value.toString();
+                if (telefone.isEmpty) {
+                  return 'Digite seu telefone.';
+                } else if (telefone.isValidPhone) {
+                  return 'Digite um telefone válido.';
+                }
+              },
+            ),
 
-                    //telefone
-                    CampoForm(
-                      hintText: 'Telefone',
-                      validator: (value) {
-                        telefone = value.toString();
-                        if (telefone.isEmpty) {
-                          return 'Digite seu telefone.';
-                        } else if (telefone.isValidPhone) {
-                          return 'Digite um telefone válido.';
-                        }
-                      },
-                    ),
-                    SizedBox(height: 20),
+            //email
+            CampoForm(
+              hintText: 'E-mail',
+              validator: (value) {
+                email = value.toString();
+                if (email.isEmpty) {
+                  return 'Digite seu email.';
+                } else if (email.isValidEmail) {
+                  return 'Digite um email valido';
+                }
+              },
+            ),
 
-                    //email
-                    CampoForm(
-                      hintText: 'E-mail',
-                      validator: (value) {
-                        email = value.toString();
-                        if (email.isEmpty) {
-                          return 'Digite seu email.';
-                        } else if (email.isValidEmail) {
-                          return 'Digite um email valido';
-                        }
-                      },
-                    ),
-                    SizedBox(height: 20),
+            //senha
+            CampoForm(
+              hintText: 'Senha',
+              validator: (value) {
+                senha = value.toString();
+                if (senha.isEmpty) {
+                  return 'Digite sua senha.';
+                }
+                if (senha.isValidPassword) {
+                  return 'Digite uma senha válida';
+                }
+              },
+            ),
 
-                    //senha
-                    CampoForm(
-                      hintText: 'Senha',
-                      validator: (value) {
-                        senha = value.toString();
-                        if (senha.isEmpty) {
-                          return 'Digite sua senha.';
-                        }
-                        if (senha.isValidPassword) {
-                          return 'Digite uma senha válida';
-                        }
-                      },
-                    ),
-                    SizedBox(height: 20),
-
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black, minimumSize: Size(400, 50)),
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            Passageiro passageiro = Passageiro(
-                                nomeCompleto: nome,
-                                cpf: cpf,
-                                telefone: telefone,
-                                email: email,
-                                senha: senha);
-
-                            passageiro.registrarPassageiro();
-
-                            Navigator.of(context).push(
-                                MaterialPageRoute(builder: (_) => LoginPage()));
-                          }
-                        },
-                        child: Text(
-                          'Avançar',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ))
-                  ],
-                ),
-              )),
+            _getButtonNext(nome, cpf, telefone, email, senha),
+            _getButtonBack(),
+          ],
         ),
-      )),
+      ),
+    );
+  }
+
+  _getText() {
+    return const Text(
+      'Cadastro de Passageiro',
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+    );
+  }
+
+  _getButtonNext(
+      String nome, String cpf, String telefone, String email, String senha) {
+    return ButtonWidget(
+      textButton: 'AVANÇAR',
+      colorTextButton: Colors.white,
+      widthButton: double.infinity,
+      borderButton: Colors.black,
+      backgroundButton: Colors.black,
+      onPressed: () {
+        if (_formKey.currentState!.validate()) {
+          Passageiro passageiro = Passageiro(
+              nomeCompleto: nome,
+              cpf: cpf,
+              telefone: telefone,
+              email: email,
+              senha: senha);
+
+          passageiro.registrarPassageiro();
+
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (_) => const LoginPassageiroPage()));
+        }
+      },
+    );
+  }
+
+  _getButtonBack() {
+    return ButtonWidget(
+      textButton: 'VOLTAR',
+      colorTextButton: Colors.black,
+      widthButton: double.infinity,
+      borderButton: Colors.black,
+      backgroundButton: Colors.white,
+      onPressed: () {
+        Navigator.pop(context);
+      },
     );
   }
 }
