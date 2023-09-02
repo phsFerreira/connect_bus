58e8c6edb95a6f9ad11f10836452d51abcc3a3fd
Jennifer Vasconcelos <64858624+jenniferVC@users.noreply.github.com>
2023-09-01 21:16:57 -0300

    Deixando arquivo main.dart responsivo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:connect_bus/home_motorista.dart';
+import 'package:connect_bus/screens/auth_page.dart';
+import 'package:connect_bus/widgets/button.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -46,64 +49,49 @@ class MainPage extends StatelessWidget {
   }
 
   _getMainPage() {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Stack(
+    return Column(
+      children: [
+        Flexible(
+          flex: 6,
+          fit: FlexFit.tight,
+          child: Stack(
             children: [
               _getImage(),
               _getTitle(),
             ],
           ),
-          getButtons(),
-        ],
-      ),
+        ),
+        Flexible(
+          flex: 1,
+          child: getButtons(),
+        ),
+      ],
     );
   }
 
   Container getButtons() {
     return Container(
       padding: const EdgeInsets.all(25.0),
-      child: Center(
+      child: const Center(
         child:
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          _buildButton('PASSAGEIRO', Colors.black, 167.0, Colors.black,
-              Colors.white, const LoginPassageiroPage()),
-          _buildButton('MOTORISTA', Colors.white, 167, Colors.black,
-              Colors.black, const LoginMotoristaPage()),
-        ]),
-      ),
-    );
-  }
-
-  // Método privado auxiliar que retorna um botão ao passar os dados por parâmetros
-  _buildButton(String textButton, Color colorTextButton, double widthButton,
-      Color borderButton, Color backgroundButton, Widget pageRedirect) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(mainPageContextKey.currentState!.context,
-            MaterialPageRoute(builder: (context) => pageRedirect));
-      },
-      child: Container(
-        height: 52.0,
-        width: widthButton,
-        padding: const EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-          color: backgroundButton,
-          border: Border.all(color: borderButton, width: 2.0),
-          borderRadius: const BorderRadius.all(Radius.circular(6.0)),
-        ),
-        child: Center(
-          child: Text(
-            textButton,
-            style: TextStyle(
-              fontSize: 13,
-              fontFamily: 'Roboto',
-              fontWeight: FontWeight.w900,
-              color: colorTextButton,
-            ),
+          ButtonWidget(
+            textButton: 'PASSAGEIRO',
+            colorTextButton: Colors.black,
+            widthButton: 167.0,
+            borderButton: Colors.black,
+            backgroundButton: Colors.white,
+            pageRedirect: LoginPassageiroPage(),
           ),
-        ),
+          ButtonWidget(
+            textButton: 'MOTORISTA',
+            colorTextButton: Colors.white,
+            widthButton: 167.0,
+            borderButton: Colors.black,
+            backgroundButton: Colors.black,
+            pageRedirect: AuthPage(),
+          ),
+        ]),
       ),
     );
   }
@@ -113,13 +101,12 @@ class MainPage extends StatelessWidget {
       'assets/images/bus_home.png',
       fit: BoxFit.cover,
       width: 600,
-      height: 700,
     );
   }
 
   _getTitle() {
     return const SizedBox(
-      height: 700,
+      // height: 700,
       child: Center(
         child: Text(
           'CONNECT BUS',
